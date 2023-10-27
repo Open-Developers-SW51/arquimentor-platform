24c9db558fd8ca68ecdf4aede73e0fe0ee51e507
leonardo <u202124304@upc.edu.pe>
2023-10-27 13:41:34 -0500

    Initial commit

---
+++ src/main/java/com/arquimentor/platform/ArquimentorPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.arquimentor.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquimentorPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquimentorPlatformApplication.class, args);
+	}
+
+}
