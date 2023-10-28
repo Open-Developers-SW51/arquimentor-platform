7206eac1edbbcd70f91fc38057cf2f1e5e8e11bc
leonardo <u202124304@upc.edu.pe>
2023-10-28 12:05:28 -0500

    feat(student): Update validation password the student.

--- src/main/java/com/arquimentor/platform/arquimentor/domain/model/valueobjects/Password.java
+++ src/main/java/com/arquimentor/platform/arquimentor/domain/model/valueobjects/Password.java
@@ -10,6 +10,11 @@ public record Password(String password) {
     // letra minúscula, al menos una letra mayúscula, al menos una
     // caracter especial sin espacios en blanco
 
+    private static final String PASSWORD_REGEX =
+            "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\\\S+$).{8,16}$";
+    private static final Pattern PASSWORD_PATTERN =
+            Pattern.compile(PASSWORD_REGEX);
+
     public Password(){
         this(null);
     }
@@ -18,6 +23,10 @@ public record Password(String password) {
         if (password == null || password.isBlank())
             throw new IllegalArgumentException("Password cannot be null or blank");
 
+        /*if (password.length()<10)
+            throw new IllegalArgumentException("Password cannot be less than 10 characters");
+        if (!PASSWORD_PATTERN.matcher(password).matches())
+            throw new IllegalArgumentException("Password isn't valid");*/
     }
 
 }
