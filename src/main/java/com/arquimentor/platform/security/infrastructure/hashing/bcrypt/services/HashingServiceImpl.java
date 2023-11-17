6080c1a4307e810f7e992339cfb99182b35100f0
JulianaYS <u20201b782@upc.edu.pe>
2023-11-17 13:18:34 -0500

    chore: added and implemented hashing service.

---
+++ src/main/java/com/arquimentor/platform/security/infrastructure/hashing/bcrypt/services/HashingServiceImpl.java
@@ -0,0 +1,24 @@
+package com.arquimentor.platform.security.infrastructure.hashing.bcrypt.services;
+
+import com.arquimentor.platform.security.application.internal.outboundservices.hashing.HashingService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HashingServiceImpl implements HashingService {
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public HashingServiceImpl() {
+        passwordEncoder = new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public String encode(CharSequence rawPassword) {
+        return passwordEncoder.encode(rawPassword);
+    }
+
+    @Override
+    public boolean matches(CharSequence rawPassword, String encodedPassword) {
+        return passwordEncoder.matches(rawPassword, encodedPassword);
+    }
+}
