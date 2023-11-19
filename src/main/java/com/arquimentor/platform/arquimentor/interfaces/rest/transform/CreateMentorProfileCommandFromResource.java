d32ea3fb2a9015bdb0fcf5f906b1d8343f87749d
leonardo <u202124304@upc.edu.pe>
2023-11-19 02:00:01 -0500

    feat(configuration): Update configuration endpoint student profile.

--- src/main/java/com/arquimentor/platform/arquimentor/interfaces/rest/transform/CreateMentorProfileCommandFromResource.java
+++ src/main/java/com/arquimentor/platform/arquimentor/interfaces/rest/transform/CreateMentorProfileCommandFromResource.java
@@ -1,10 +1,7 @@
 package com.arquimentor.platform.arquimentor.interfaces.rest.transform;
 
 import com.arquimentor.platform.arquimentor.domain.model.commands.CreateMentorProfileCommand;
-import com.arquimentor.platform.arquimentor.domain.model.commands.CreateStudentProfileCommand;
-import com.arquimentor.platform.arquimentor.domain.model.valueobjects.PhoneNumber;
 import com.arquimentor.platform.arquimentor.interfaces.rest.resources.CreateMentorProfileResource;
-import com.arquimentor.platform.arquimentor.interfaces.rest.resources.CreateStudentProfileResource;
 
 public class CreateMentorProfileCommandFromResource {
     public static CreateMentorProfileCommand resourceToCommand(CreateMentorProfileResource resource) {
